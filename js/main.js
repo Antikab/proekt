a4f211edce84d30cc7f23bcf2b552fe400fc680d
Arseny Prosvetov <arsprosvet@mail.ru>
2022-08-29 00:58:13 +0300

    g

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const myNiggas = {
+	nigerBand: { power: ['Big Smoke', 'ff'], fast: 'speed' },
+	cars: 'opel corsa'
+};
+
+console.log(myNiggas)
+
+
+
+console.log(`Печать всех будильников: ${myNiggas.nigerBand.fast.length}`);
